defce9d90a505b2e1c939b49bbfb9ff6532e8fcd
Diego O <diosuna@redelcid.com>
2019-05-02 15:35:03 -0600

    Card component created

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <h1 class="contact-card">Mi Lista de Contactos</h1>
+    <MiComponente 
+      v-for="amigo in amigos" :key="amigo.id"
+      :amigo="amigo" />
+    
+  </main>
+</template>
+  
+<script>
+import TarjetaContacto from '@/components/TarjetaContacto.vue';
+
+export default {
+  name: 'app',
+  components: {
+    MiComponente: TarjetaContacto
+  },
+  data(){
+    return{
+      amigos: [
+        {
+          id: 1,
+          nombre: 'Rodrigo',
+          descripcion: 'Desarrollador de software',
+          imgSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        },
+        {
+          id: 2,
+          nombre: 'Fernando',
+          descripcion: 'Lider de Tomato',
+          imgSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        },
+        {
+          id: 3,
+          nombre: 'Alan',
+          descripcion: 'Amigo de Fernando',
+          imgSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        },
+        {
+          id: 4,
+          nombre: 'Luis',
+          descripcion: 'Mentor de Angular',
+          imgSrc: 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png',
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+ 
+</style>
---
+++ src/components/TarjetaContacto.vue
@@ -0,0 +1,29 @@
+<template>
+  <main>    
+    <section class="contact-card">
+      <img :src="amigo.imgSrc" alt="Imagen de perfil">
+      <h2>{{ amigo.nombre }}</h2>
+      <p>{{ amigo.descripcion }}</p>      
+    </section>    
+  </main>
+</template>
+
+<script>
+export default {
+  props: ['amigo']
+}
+</script>
+
+<style scoped>
+   .contact-card img{
+    width: 100px;
+    margin: 0 auto;
+  }
+  .contact-card {
+    padding: 30px;
+    margin: 30px;
+    -webkit-box-shadow: -5px 10px 5px 0px rgba(0,0,0,0.118);
+    -moz-box-shadow: -5px 10px 5px 0px rgba(0,0,0,0.118);
+    box-shadow: -5px 10px 5px 0px rgba(0,0,0,0.118);
+  }
+</style>
